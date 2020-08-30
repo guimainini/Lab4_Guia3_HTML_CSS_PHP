4fb25c511a511c4c78d0b0d8949833bfcf581cf0
guimainini <guimaini@gmail.com>
2020-08-30 11:14:50 -0300

    Ejercicios 1 y 2

---
+++ guiaTres.php
@@ -0,0 +1,73 @@
+<?php
+
+echo "Ejercicio 1<br><br>";
+
+$myNumber = 123;
+$numberString = "123";
+
+echo "a) ";
+$total = $myNumber + $numberString;
+
+echo "El total es : $total<br>";
+
+echo "b)";
+$total = $numberString + $myNumber;
+echo "El total a la inversa es : $total<br>";
+
+echo "c)";
+$total = $myNumber.$numberString;
+echo "El total es : $total";
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+echo "<br><br><br>";
+echo "Ejercicio 2<br><br>";
+echo "a)<br>";
+if(1 == "1"){
+    echo "It´s right"; 
+}
+echo "<br>b)<br>";
+if(1 === "1"){
+    echo "It´s right"; 
+}
+echo "<br>c)<br>";
+if(!null){
+    echo "It´s right"; 
+}
+echo "<br>d)<br>";
+if(!false ){
+    echo "It´s right"; 
+}
+echo "<br>e)<br>";
+if(""){
+    echo "It´s right"; 
+}
+echo "<br>f)<br>";
+if(" "){
+    echo "It´s right"; 
+}
+echo "<br>g)<br>";
+if("tested"){
+    echo "It´s right"; 
+}
+echo "<br>h)<br>";
+if(1-1){
+    echo "It´s right"; 
+}
+
+////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////
+echo "<br><br><br>";
+echo "Ejercicio 3<br><br>";
+
+
+
+
+
+
+
+
+
+
+
+
+?>
